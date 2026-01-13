625ca93c57c165cdc9ae8dae1dc5f40d8a2a282d
IOT-김태현 <kth9903@gmail.com>
2026-01-13 16:51:08 +0900

    refactor: user 구조체 password null 허용 및 소셜 관련 필드 추가

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,7 +5,15 @@ import "time"
 type User struct {
 	ID            uint      `gorm:"primaryKey" json:"id"`
 	Email         string    `gorm:"uniqueIndex;not null;size:255" json:"email"`
-	PasswordHash  string    `gorm:"not null;size:255" json:"-"` // json:"-"는 JSON 응답에서 제외
+	//PasswordHash *string: not null -> null 허용 변경(소셜 로그인 유저 비번 X)
+	/*
+	(Pointer 사용 이유): Go 언어에서 string 타입의 제로값은 "" (빈 문자열)입니다. 
+	하지만 DB에서는 NULL과 ""은 다릅니다. 
+	string (포인터)를 사용해야 nil일 때 DB에 진짜 NULL이 들어갑니다.
+	*/
+	PasswordHash *string    `gorm:"size:255" json:"-"` // json:"-"는 JSON 응답에서 제외, 
+	Provider string `gorm:"not null;default:'email',size:20;index:idx_provider_social_id" json:"provider"`
+	SocialID string `gorm:"size:255;index:idx_provider_social_id" json:"social_id"`
 	EmailVerified bool      `gorm:"default:false" json:"email_verified"`
 	CreatedAt     time.Time  `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
